ebca114773bfc394623099127948f9334e989b90
Johan2511 <johanpenuelah@gmail.com>
2023-12-12 15:00:12 -0500

    Se consumio la api de fakeStore para pintarla en el componente card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,39 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home () {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
   return (
     <>
       <Layout>
-        Home
-        <Card />
+        Home 
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map( item  => (
+            <Card 
+              key={item.id} data={item} />
+          )) 
+        }
+        </div>
       </Layout>  
     </>
-  )
+  );
 }
 
 export default Home
