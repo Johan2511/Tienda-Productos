d92816fef6ed3d7e466798d359dcac73344ffa6e
Johan2511 <johanpenuelah@gmail.com>
2024-02-27 16:17:28 -0500

    Se agrego al contexto el estado de cerrar y abrir el ProductDetail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -8,7 +8,9 @@ const Card = (data) => {
     const context = useContext(ShoopingCartContext)
 
   return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+    <div 
+    className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+    onClick={() => context.openProductDetail()}>
         <figure className='relative mb-2 w-full h-4/5'>
             <span className=' absolute bottom-0 left-0 bg-amber-900/60 rounded-lg text-white text-xs m-2 px-3 py-0.5'>
                 {data.data.category}
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,19 @@ export const ShoopingCartContext = createContext()
 
 export const ShoopingCartProvider = ({children}) => {
     const [count, setCount] = useState(0)
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     console.log('COUNT: ', count);
+
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
 
     return (
         <ShoopingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen
         }}>
             { children }
         </ShoopingCartContext.Provider>  
