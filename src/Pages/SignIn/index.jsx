b484f8e241bb4e7aeef4be358bc031c50826fbf4
Johan2511 <johanpenuelah@gmail.com>
2024-03-05 17:19:25 -0500

    Se agrego funcionalidad para saber si el usuario tiene cuenta o no para habilitar o desabilitar boton tambien se agrego estado local para mostrar la vista de crear cuenta o hacer log In

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,11 +1,25 @@
+import { useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { ShoopingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
 function SignIn() {
-  return (
-      <Layout>
-        <div className="flex justify-center items-center h-screen">
+
+  const context = useContext(ShoopingCartContext)
+  const [view, setView ] = useState('user-info')
+
+  //Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+  //trae una cuenta
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalStage = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalStage
+
+  const renderLogIn = () => {
+    return (
+      <div className="flex justify-center items-center">
           <div className="w-full max-w-md">
-            <h1 className="text-3xl font-semibold mb-6 text-center">Welcome</h1>
             <form className=" bg-amber-400 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 w-80 h-80">
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -17,8 +31,8 @@ function SignIn() {
                   name="email"
                   type="email"
                   required
-                  placeholder='Correofalso123@gmail.com'
                 />
+                {parsedAccount?.email}
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -30,18 +44,22 @@ function SignIn() {
                   name="password"
                   type="password"
                   required
-                  placeholder='******'
                 />
+                {parsedAccount?.password}
               </div>
               <div className="flex flex-col items-center justify-between ">
                 <button
                   className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mb-2 rounded focus:outline-none focus:shadow-outline w-full"
                   type="button"
                   onClick={() => alert('Lógica de inicio de sesión aquí')}
+                  disabled={!hasUserAnAccount}
                 >
-                  Sign In
+                  Log In
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" href="#" onClick={() => alert('Redirigir a la página de registro aquí')}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" 
+                href="#" 
+                onClick={() => setView('create-user-info')}
+                disabled={hasUserAnAccount}>
                 Sign up
               </button>
               </div>
@@ -55,6 +73,22 @@ function SignIn() {
             </div>
           </div>
         </div>
+    )
+  }
+  const renderCreateUserInfo = () => {
+    // TODO: Create render view
+  }
+
+  const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn() 
+
+  return (
+      <Layout>
+            <div className='flex items-center justify-center relative w-80 mb-4'>
+            <h1 className='font-medium text-xl'>
+              Welcome 
+            </h1>
+          </div>
+        {renderView()}
     </Layout>
   )
 }
